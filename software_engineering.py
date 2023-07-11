36cb18fc37847e07a472880c2a29abad83689251
Alissa Adams <alissaadams@Alissas-MBP.lan>
2023-07-11 10:56:31 -0400

    encode done

--- software_engineering.py
+++ software_engineering.py
@@ -1,16 +1,16 @@
-# def encode(password):
-#     string = ''
-#     for element in password:
-#         int(element)
-#         element = element + 3
-#         string = string + str(element)
-#     return string
-#
-# def decode(encoded_password):
-#     pass
-#
-# def main():
-#     pass
-#
-# if __name__ == '__main__':
-#     print(encode('12345678'))
+def encode(password):
+    string = ''
+    for element in password:
+        element = int(element) + 3
+        string = string + str(element)
+
+    return string
+
+def decode(encoded_password):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    print(encode('12345678'))
